f0eef9281fd1265b4fe63925b5ac51ad5f971389
Asya Brusina <asya.brusina@gmail.com>
2023-12-20 21:06:54 +0300

    Draw rough draft vesrion of sort-fitlter control panel.

--- TaskTrackerDX/ViewModels/MainViewModel.cs
+++ TaskTrackerDX/ViewModels/MainViewModel.cs
@@ -6,8 +6,19 @@ namespace TaskTrackerDX.ViewModels
 {
   public partial class MainViewModel : NotifyObject
   {
-    public TaskEditViewModel TaskEditContext { get => _taskEditContext; set { _taskEditContext = value; OnPropertyChanged(); } }
+    public TaskEditViewModel TaskEditContext
+    { 
+      get => _taskEditContext;
+      set { _taskEditContext = value; OnPropertyChanged(); }
+    }
     private TaskEditViewModel _taskEditContext = new TaskEditViewModel();
+
+    public SortFilterSettingsViewModel SortFilterContext 
+    {
+      get => _sortFilterContext;
+      set { _sortFilterContext = value;OnPropertyChanged(); }
+    }
+    private SortFilterSettingsViewModel _sortFilterContext;
 
     public MainViewModel()
     {
